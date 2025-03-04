bf2e6039dfdc5bc1132843758e247e244d770b69
pranavreddypajjuru <pranavreddy165@gmail.com>
2025-03-03 22:49:21 -0500

    Working on all the features of Collections

--- Collections/HashSetImplMethods.java
+++ Collections/HashSetImplMethods.java
@@ -19,7 +19,29 @@ public class HashSetImplMethods {
         s.clear();
         System.out.println("After using clear method: "+s);
 
-        
+        System.out.println("Is the HashSet Object empty:"+s.isEmpty());
+        s.add(15);
+        s.add(1);
+        s.add(1000);
+        s.add(150);
+        s.add(51);
+        s.add(66);
+        System.out.println(s);
+        System.out.println("Does HashSet contain 150 "+s.contains(150));
+        System.out.println("Size of the HashSet:"+s.size());
+
+        Set<Integer> s1 = new HashSet<>();
+        s1.add(1);
+        s1.add(15);
+        System.out.println("s:"+s);
+        System.out.println("s1:"+s1);
+        System.out.println("DOes s contains all the elements of s1: "+s.containsAll(s1));
+        s.retainAll(s1);
+        System.out.println("s:"+s);
+        s.removeAll(s1);
+        System.out.println("s:"+s);
+
+
 
     }
 }
